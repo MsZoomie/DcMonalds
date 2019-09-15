2d14d7f85e062de152dd8c73587108a987daea15
Sara Uvalic <sara.uvalic@hotmail.com>
2019-09-15 16:30:11 +0200

    Started working on pathfinder AI

---
+++ DcMonalds/Assets/Scripts/Pathfinder Project/Terrain.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrain : MonoBehaviour
+{
+    List <GameObject> Tiles = new List<GameObject> ();
+
+    void Awake()
+    {
+        BoxCollider[] temp = GetComponentsInChildren<BoxCollider>();
+        foreach (var tile in temp)
+        {
+            Tiles.Add(tile.gameObject);    
+            Debug.Log(tile.name);       
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+}
