b8d8e0ae54e3857dcc426ac6729fba2225538596
Sara <sara.uvalic@hotmail.com>
2019-09-21 17:19:04 +0200

    Working on car behavior

---
+++ DcMonalds/Assets/Scripts/CarBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBehaviour : MonoBehaviour
+{
+    public float delay = 0;
+    public int distanceToTravel;
+    public float speed = 1.0f;
+
+    private Movement movement;
+   
+    private void Awake()
+    {
+        movement = gameObject.GetComponent<Movement>();
+        SetMovementValues();
+    }
+
+    private void Start()
+    {
+        StartCoroutine( WaitForDelay());
+        
+    }
+    private void SetMovementValues()
+    {
+        Vector3 temp = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        movement.SetStartMarker(temp);
+        temp = new Vector3(transform.position.x, transform.position.y, transform.position.z - distanceToTravel);
+        movement.SetEndMarker(temp);
+        movement.alwaysLerping = false;
+        movement.turn = false;
+        movement.lerpSpeed = speed;
+    }
+
+    IEnumerator WaitForDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        movement.StartLerp(false);
+    }
+  
+}
