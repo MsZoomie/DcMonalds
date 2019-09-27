cb12615410a2c7feb86b5e3c522f24c5bb7e316e
Sara <sara.uvalic@hotmail.com>
2019-09-27 09:54:10 +0200

    Made CameraMovement script, tried to build to android, but will not work on school computer?

---
+++ DcMonalds/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private void Update()
+    {
+        Move();
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+
+    private void Move()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+
+                Vector2 pos = touch.position;
+                transform.position = new Vector3(-pos.x, pos.y, 0.0f);
+            }
+        }
+    }
+
+}
