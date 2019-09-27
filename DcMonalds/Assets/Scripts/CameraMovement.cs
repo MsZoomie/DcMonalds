163887d03c2f9eb0f3c242a9703f981762a88e2b
Sara <sara.uvalic@hotmail.com>
2019-09-27 13:09:19 +0200

    Camera movement working, a little bit weird tho

--- DcMonalds/Assets/Scripts/CameraMovement.cs
+++ DcMonalds/Assets/Scripts/CameraMovement.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    public float movementScale = 1.0f;
 
-    Vector3 startPos;
-    Vector3 endPos;
+    public Vector3 currentPos;
+    public Vector3 nextPos;
 
- /*   bool moving;
-    public float speed = 1.0f;
-    private float startTime;
-    private float journeyLength;*/
+    public Vector3 originalPos;
+    public Vector3 normal;
+
+    /*   bool moving;
+       public float speed = 1.0f;
+       private float startTime;
+       private float journeyLength;*/
 
 
-   
+    private void Awake()
+    {
+        originalPos = transform.position;
+
+        Vector3 temp = originalPos + Vector3.forward + Vector3.right;
+        normal = Vector3.Cross(originalPos, temp);
+    }
+
     private void LateUpdate()
     {
         Move();
@@ -35,15 +46,25 @@ public class CameraMovement : MonoBehaviour
             switch (touch.phase)
             {
                 case TouchPhase.Began:
-                    startPos = touch.position;
-            //        moving = true;
-            //        startTime = Time.time;
+                    currentPos = touch.position;
+                    currentPos = Camera.main.ScreenToWorldPoint(currentPos);
+                    nextPos = touch.position;
+                    nextPos = Camera.main.ScreenToWorldPoint(nextPos);
                     break;
                 case TouchPhase.Moved:
-                    endPos = touch.position;
+                    currentPos = nextPos;
+                    nextPos = touch.position;
+                    nextPos = Camera.main.ScreenToWorldPoint(nextPos);
 
-                    transform.position += new Vector3(endPos.x - startPos.x, transform.position.y, endPos.z - startPos.z);
+                    Vector2 temp = nextPos - currentPos;
+                    Vector3 tempX = Vector3.right * temp.x;
+                    tempX = Vector3.ProjectOnPlane(tempX, transform.up);
 
+                    Vector3 tempY = Vector3.forward * temp.y;
+                    tempY = Vector3.ProjectOnPlane(tempY, transform.right);
+
+                    transform.position += tempX.normalized * movementScale;
+                    transform.position += tempY.normalized * movementScale;
 
                     break;
                 case TouchPhase.Ended:
@@ -55,6 +76,35 @@ public class CameraMovement : MonoBehaviour
             }
 
         }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            currentPos = Input.mousePosition;
+            currentPos = Camera.main.ScreenToWorldPoint(currentPos);
+            nextPos = Input.mousePosition;
+            nextPos = Camera.main.ScreenToWorldPoint(nextPos);
+        }
+        else if (Input.GetKey(KeyCode.Mouse0))
+        {
+            currentPos = nextPos;
+            nextPos = Input.mousePosition;
+            nextPos = Camera.main.ScreenToWorldPoint(nextPos);
+
+            Vector2 temp = nextPos - currentPos;
+            //  Debug.Log(temp);
+
+            Vector3 tempX = Vector3.right * temp.x;
+            tempX = Vector3.ProjectOnPlane(tempX, transform.up);
+            Debug.Log(tempX);
+
+            Vector3 tempY = Vector3.forward * temp.y;
+            tempY = Vector3.ProjectOnPlane(tempY, transform.right);
+
+            transform.position += tempX.normalized * movementScale;
+            transform.position += tempY.normalized * movementScale;
+
+        }
+     
     }
 
 }
