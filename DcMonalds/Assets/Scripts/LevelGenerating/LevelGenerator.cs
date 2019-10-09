a69fd4f2b67462a8e5f2500c8aadb3521d0e7cb7
Sara <sara.uvalic@hotmail.com>
2019-10-09 17:52:04 +0200

    Jag kan generera extrasaker!

--- DcMonalds/Assets/Scripts/LevelGenerating/LevelGenerator.cs
+++ DcMonalds/Assets/Scripts/LevelGenerating/LevelGenerator.cs
@@ -17,7 +17,7 @@ public class LevelGenerator : MonoBehaviour
     [Space]
     public Pathfinder pathfinder;
 
-    
+    private GameObject level;
     private GameObject levelObstacles;
 
     private SearchSpace searchSpace;
@@ -69,7 +69,7 @@ public class LevelGenerator : MonoBehaviour
         }
 
 
-        GameObject level = new GameObject("Level");
+        level = new GameObject("Level");
         level.AddComponent<Level>();
         searchSpace = level.AddComponent<SearchSpace>();
 
@@ -180,9 +180,11 @@ public class LevelGenerator : MonoBehaviour
                 }
 
             }
+        }
 
-
-           // Debug.Log("The level is possible: " + validLevel);
+        if (theme.extras.Count > 0)
+        {
+            InstantiateExtras();
         }
 
 
@@ -232,6 +234,7 @@ public class LevelGenerator : MonoBehaviour
 
         return true;
     }
+
 
     /// <summary>
     /// Choose which obstacle to be spawned.
@@ -299,6 +302,66 @@ public class LevelGenerator : MonoBehaviour
         return null;
     ObstacleChosen:
         return obstacle;
+    }
+
+
+    private void InstantiateExtras()
+    {
+        GameObject extras = new GameObject("Extras");
+        extras.transform.SetParent(level.transform);
+
+
+        for (int i = 0; i < theme.extras.Count; i++)
+        {
+            Extra instance = theme.extras[i];
+
+            float x = numberOfLanes / instance.interval.x;
+            float y = numberOfRows / instance.interval.y;
+
+            GameObject instanceParent = new GameObject("Extra" + i.ToString());
+            instanceParent.transform.SetParent(extras.transform);
+
+
+            if (instance.repeatingX && instance.repeatingY)
+            {
+                for (int rowIndex = 0; rowIndex < y; rowIndex++)
+                {
+                    for (int laneIndex = 0; laneIndex < x; laneIndex++)
+                    {
+                        Vector3 pos = new Vector3(instance.startPos.x + (instance.interval.x * laneIndex), instance.startPos.y, instance.startPos.z + (instance.interval.y * rowIndex));
+                        GameObject extra = Instantiate<GameObject>(instance.prefab, pos, Quaternion.identity);
+                        extra.name = instance.prefab.name;
+                        extra.transform.SetParent(instanceParent.transform);
+                    }
+                }
+            }
+            else if (instance.repeatingX)
+            {
+                for (int laneIndex = 0; laneIndex < x; laneIndex++)
+                {
+                    Vector3 pos = new Vector3(instance.startPos.x + (instance.interval.x * laneIndex), instance.startPos.y, instance.startPos.z);
+                    GameObject extra = Instantiate<GameObject>(instance.prefab, pos, Quaternion.identity);
+                    extra.name = instance.prefab.name;
+                    extra.transform.SetParent(instanceParent.transform);
+                }
+            }
+            else if (instance.repeatingY)
+            {
+                for (int rowIndex = 0; rowIndex < y; rowIndex++)
+                {
+                    Vector3 pos = new Vector3(instance.startPos.x, instance.startPos.y, instance.startPos.z + (instance.interval.y * rowIndex));
+                    GameObject extra = Instantiate<GameObject>(instance.prefab, pos, Quaternion.identity);
+                    extra.name = instance.prefab.name;
+                    extra.transform.SetParent(instanceParent.transform);
+                }
+            }
+            else
+            {
+                GameObject extra = Instantiate<GameObject>(instance.prefab, instance.startPos, Quaternion.identity);
+                extra.name = instance.prefab.name;
+                extra.transform.SetParent(instanceParent.transform);
+            }
+        }
     }
 
 
--- DcMonalds/Assets/Scripts/LevelGenerating/LevelTheme.cs
+++ DcMonalds/Assets/Scripts/LevelGenerating/LevelTheme.cs
@@ -10,62 +10,7 @@ public class LevelTheme : ScriptableObject
 {
     public List<LaneInfo> lanes = new List<LaneInfo>();
 
-    private GameObject sidewalkPrefab;
-    private GameObject grassPrefab;
-    private GameObject roadPrefab;
-
-    private void Awake()
-    {
-        sidewalkPrefab = Resources.Load<GameObject>("LanePrefabs/Sidewalk");
-        grassPrefab = Resources.Load<GameObject>("LanePrefabs/Grass");
-        roadPrefab = Resources.Load<GameObject>("LanePrefabs/Road");
-
-
-        for (int i = 0; i < lanes.Count; i++)
-        {
-            switch (lanes[i].type)
-            {
-                case LaneInfo.LaneType.Sidewalk:
-                    lanes[i].SetPrefab(sidewalkPrefab);
-                    break;
-                case LaneInfo.LaneType.Grass:
-                    lanes[i].SetPrefab(grassPrefab);
-                    break;
-                case LaneInfo.LaneType.Road:
-                    lanes[i].SetPrefab(roadPrefab);
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    private void OnValidate()
-    {
-        sidewalkPrefab = Resources.Load<GameObject>("LanePrefabs/Sidewalk");
-        grassPrefab = Resources.Load<GameObject>("LanePrefabs/Grass");
-        roadPrefab = Resources.Load<GameObject>("LanePrefabs/Road");
-
-
-        for (int i = 0; i < lanes.Count; i++)
-        {
-            switch (lanes[i].type)
-            {
-                case LaneInfo.LaneType.Sidewalk:
-                    lanes[i].SetPrefab(sidewalkPrefab);
-                    break;
-                case LaneInfo.LaneType.Grass:
-                    lanes[i].SetPrefab(grassPrefab);
-                    break;
-                case LaneInfo.LaneType.Road:
-                    lanes[i].SetPrefab(roadPrefab);
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
+    public List<Extra> extras = new List<Extra>();
 }
 
 
@@ -73,24 +18,23 @@ public class LevelTheme : ScriptableObject
 [Serializable]
 public class LaneInfo
 {
-    
-
-    public enum LaneType
-    { Sidewalk, Grass, Road }
-
-    public LaneType type;
+    public GameObject laneTypePrefab;
+    [Space]
     public List<ObstacleInstance> obstacles = new List<ObstacleInstance>();
-
-    private GameObject prefab;
-
-    public void SetPrefab(GameObject prefabObject)
-    {
-        prefab = prefabObject;
-    }
 
     public GameObject GetPrefab()
     {
-        return prefab;
+        return laneTypePrefab;
     }
+}
+
+[Serializable]
+public class Extra
+{
+    public GameObject prefab;
+    public bool repeatingX;
+    public bool repeatingY;
+    public Vector2 interval;
+    public Vector3 startPos;
 }
 
